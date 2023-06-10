9431b1eb9eaf79a759a1352b5e34a776419cdd69
Bilal <88664105+Bilal-11@users.noreply.github.com>
2023-06-10 07:18:27 +0530

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(text,times) {
+    if(text === '' || times === 0)
+        return '';
+    else if (times < 0)
+        return "ERROR";
+    else
+    {
+        let returnText = '';
+        for(let i = 1; i <= times ; i++)
+        {
+            returnText += text;
+        }
+        return returnText;
+    }
 };
 
 // Do not edit below this line
