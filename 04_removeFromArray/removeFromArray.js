eb3331194e15b642d90acd28569b87610dab29e6
Bilal <88664105+Bilal-11@users.noreply.github.com>
2023-06-10 08:06:02 +0530

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...remItems) {
+    for(const item of remItems)
+    {
+        let index = arr.indexOf(item);        
+        if(index > -1)
+        {
+            arr.splice(index,1);
+        }
+        
+    }
+    return arr;
 };
 
 // Do not edit below this line
